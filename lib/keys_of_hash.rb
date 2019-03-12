c5797343ed014258b31aefab6047026d2c616107
Milan <milan.dhar@gmail.com>
2019-03-12 16:00:16 -0700

    monkey

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    new_array = []
+    arguments.each do |argument|
+      i=0
+    values.each do |country|
+      if country == argument
+        new_array << keys[i]
+      end
+      i+=1
+    end
   end
+    new_array
+end
 end
